215f943993d492473c8c4bfcaa034e4dac1c728e
Randy Davis <randy_davis@outlook.com>
2015-02-07 13:55:08 -0600

    finish model inheritance section

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-var Song = Backbone.Model.extend({
-  validate: function(attr){
-    if(!attr.title)
-      return "Title is required";
+var Animal = Backbone.Model.extend({
+  walk: function() {
+    console.log("Animal walking...");
   }
 });
 
-var song = new Song();
-// Checking the console at this point with the following returns:
-  // song.isValid(); // false
-  // song.validationError; // "Title is required"
+var Dog = Animal.extend({
+  walk: function() {
+    Animal.prototype.walk.apply(this);
 
-song.set('title', 'Lets Get It On');
-// Checking console now returns:
-  // song.isValid(); // true
+    console.log("Dog walking...");
+  }
+});
 
-  
+var dog = new Dog();
+
+dog.walk();

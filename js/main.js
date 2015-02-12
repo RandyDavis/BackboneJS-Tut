3d3de82d6ba35a376ec26b1e0a0894abaf08251d
Randy Davis <randy_davis@outlook.com>
2015-02-11 22:41:18 -0600

    finish Passing Data to Views section

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
+var Song = Backbone.Model.extend();
+
+var Songs = Backbone.Collection.extend({
+  model: Song
+});
 var SongView = Backbone.View.extend({
-
-  tagName: "span",
-
-  className: "song",
-
-  id: "1234",
-
-  attributes: {
-    "data-genre": "Jazz"
-  },
+  tagName: "li",
 
   render: function() {
-    this.$el.html("Hello World!  I came from the  render() function.");
+    this.$el.html(this.model.get("title"));
 
     return this;
   }
 });
 
-var songView = new SongView();
-songView.render();
+var songs = new Songs([
+  new Song({ title: "Blue in Green" }),
+  new Song({ title: "So What" }),
+  new Song({ title: "All Blues" })
+]);
 
-$("#container").html(songView.render().$el);
+var SongsView = Backbone.View.extend({
+  render: function() {
+    var self = this;
+    this.model.each(function(song) {
+      var songView = new SongView({ model: song });
+      self.$el.append(songView.render().$el);
+    });
+  }
+});
+
+var songsView = new SongsView({ el: "#songs", model: songs });
+songsView.render();

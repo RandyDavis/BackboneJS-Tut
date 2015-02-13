e20a5a1c5f70266db63f6e4f2657597d6b299c19
Randy Davis <randy_davis@outlook.com>
2015-02-12 23:57:02 -0600

    add first 2 button click event example with generic button

--- js/main.js
+++ js/main.js
@@ -1,33 +1,27 @@
 var Song = Backbone.Model.extend();
 
-var Songs = Backbone.Collection.extend({
-  model: Song
-});
 var SongView = Backbone.View.extend({
-  tagName: "li",
+  events: {
+    "click": "onClick",
+    "click .bookmark": "onClickBookmark"
+  },
+
+  onClick: function() {
+    console.log("Listen Clicked!");
+  },
+
+  onClickBookmark: function() {
+    console.log("Bookmark Clicked");
+  },
 
   render: function() {
-    this.$el.html(this.model.get("title"));
-
+    this.$el.html(this.model.get("title") + " <button>Listen</button> <button class='bookmark'>Bookmark</button>");
     return this;
   }
 });
 
-var songs = new Songs([
-  new Song({ title: "Blue in Green" }),
-  new Song({ title: "So What" }),
-  new Song({ title: "All Blues" })
-]);
+var song = new Song({ title: "Blue in Green" });
 
-var SongsView = Backbone.View.extend({
-  render: function() {
-    var self = this;
-    this.model.each(function(song) {
-      var songView = new SongView({ model: song });
-      self.$el.append(songView.render().$el);
-    });
-  }
-});
+var songView = new SongView({ el: "#container", model: song });
 
-var songsView = new SongsView({ el: "#songs", model: songs });
-songsView.render();
+songView.render();

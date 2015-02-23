159d819131052a44199491e53f6fa824073e43c4
Randy Davis <randy_davis@outlook.com>
2015-02-23 00:08:46 -0600

    finish redo of handling DOM events lesson

--- js/main.js
+++ js/main.js
@@ -22,43 +22,108 @@
 
 
 // Lecture 24 - Passing Model Data to Views
+// var Song = Backbone.Model.extend();
+
+// var Songs = Backbone.Collection.extend({
+//   model: Song
+// });
+
+// var SongView = Backbone.View.extend({
+//   tagName: "li",
+//   render: function() {
+//     this.$el.html(this.model.get("title"));
+
+//     return this;
+//   }
+// });
+
+// var SongsView = Backbone.View.extend({
+//   render: function() {
+//     var self = this;
+//     this.model.each(function(song) {
+//       var songView = new SongView({ model: song });
+//       self.$el.append(songView.render().$el);
+//     });
+//   }
+// });
+
+// var song = new Song({ title: "Blue in Green" });
+
+// var songs = new Songs([
+//   new Song({ title: "Blue in Green" }),
+//   new Song({ title: "Home" }),
+//   new Song({ title: "Read My Mind" })
+// ]);
+
+// // Render the Song View
+// var songView = new SongView({ el: "#container", model: song });
+// songView.render();
+
+// // Render the Collection List View
+// var songsView = new SongsView({ el: "#songs", model: songs });
+// songsView.render();
+
+
+
+
+// Lecture 25 - Handling DOM Events
 var Song = Backbone.Model.extend();
 
-var Songs = Backbone.Collection.extend({
-  model: Song
-});
-
 var SongView = Backbone.View.extend({
-  tagName: "li",
-  render: function() {
-    this.$el.html(this.model.get("title"));
+  events: {
+    "click": "onClick",
+    "click .bookmark": "onClickBookmark"
+  },
 
+  onClick: function() {
+    console.log("Listen clicked!");
+  },
+
+  onClickBookmark: function(e) {
+    e.stopPropagation();
+    console.log("Bookmark clicked!");
+  },
+
+  render: function() {
+    this.$el.html(this.model.get("title") + " <button>Listen</button> <button class='bookmark'>Bookmark</button>");
     return this;
   }
 });
 
-var SongsView = Backbone.View.extend({
-  render: function() {
-    var self = this;
-    this.model.each(function(song) {
-      var songView = new SongView({ model: song });
-      self.$el.append(songView.render().$el);
-    });
-  }
-});
-
 var song = new Song({ title: "Blue in Green" });
-
-var songs = new Songs([
-  new Song({ title: "Blue in Green" }),
-  new Song({ title: "Home" }),
-  new Song({ title: "Read My Mind" })
-]);
-
-// Render the Song View
 var songView = new SongView({ el: "#container", model: song });
 songView.render();
 
-// Render the Collection List View
-var songsView = new SongsView({ el: "#songs", model: songs });
-songsView.render();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

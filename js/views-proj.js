14bec905cc02895e7eaeb533c398c07c307b4215
Randy Davis <randy_davis@outlook.com>
2015-02-25 22:29:21 -0600

    continue working on backbone views project

--- js/views-proj.js
+++ js/views-proj.js
@@ -13,9 +13,17 @@ var VehicleView = Backbone.View.extend({
   //   this.model.on("remove", this.onVehicleRemoved, this);
   // },
   tagName: 'li',
+
+  className: 'vehicle',
+
+  attributes: {
+    "data-color": "red"
+  },
+
   render: function() {
     this.$el.html(this.model.get("registrationNumber"));
     this.$el.attr("id", this.model.id);
+    return this;
   }
 });
 
@@ -27,17 +35,19 @@ var VehiclesView = Backbone.View.extend({
   onVehicleRemoved: function() {
     console.log("Vehicle Removed");
   },
-  className: 'vehicle',
-  attributes: {
-    "data-color": "red"
-  },
+
   events: {
     "click #removeVehicle": "onVehicleRemoved"
   },
   render: function() {
-    var html = $("#vehicleTemplate").html();
-    var template = _.template(html);
-    this.$el.html(template(this.model.toJSON()));
+    var self = this;
+    this.model.each(function(vehicle) {
+      var vehicleView = new VehicleView({ model: vehicle });
+      self.$el.append(vehicleView.render().$el);
+    });
+    var template = _.template($("#vehicleTemplate").html());
+    var html = template(this.model.toJSON());
+    // this.$el.html(template(this.model.toJSON()));
     return this;
   }
 
@@ -51,6 +61,9 @@ var vehicles = new Vehicles([
 
 // Remove this comment
 //  Another comment
+
+// Need to add code to get list of vehicles to show on .render() call
+
 
 
 var vehiclesView = new VehicleView({ el: "#vehicles", model: vehicles});

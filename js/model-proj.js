26559c9fb5e8d4bae6ac58e14972722a3945e660
Randy Davis <randy_davis@outlook.com>
2015-05-15 00:58:13 -0500

    redo model-project

--- js/model-proj.js
+++ js/model-proj.js
@@ -1,25 +1,51 @@
+// var Vehicle = Backbone.Model.extend({
+//   idAttribute: "registrationNumber",
+//   validate: function(registrationNumber) {
+//     if(!registrationNumber) {
+//       return "A registration number is required";
+//     }
+//   },
+//   urlRoot: "/api/vehicles",
+//   defaults: {
+//     start: function(){
+//       console.log("Vehicle started.");
+//     }
+//   }
+// });
+
+// var Car = Vehicle.extend({
+//   start: function(){
+//     console.log("Car with registration number " + this.get('registrationNumber') + " started.");
+//   }
+// });
+
+// var car = new Car({
+//   registrationNumber: "XLI887",
+//   color: "Blue"
+// });
+
+// Another Attempt for Models Project
 var Vehicle = Backbone.Model.extend({
   idAttribute: "registrationNumber",
-  validate: function(registrationNumber) {
-    if(!registrationNumber) {
-      return "A registration number is required";
+  validate: function(attrs) {
+    if(!attrs.registrationNumber) {
+      return "A registration number is required!";
     }
   },
   urlRoot: "/api/vehicles",
   defaults: {
-    start: function(){
-      console.log("Vehicle started.");
+    start: function() {
+      console.log("Vehicle started...");
     }
   }
 });
 
 var Car = Vehicle.extend({
-  start: function(){
+  start: function() {
     console.log("Car with registration number " + this.get('registrationNumber') + " started.");
   }
 });
-
 var car = new Car({
-  registrationNumber: "XLI887",
+  registrationNumber: "NEW-REG21",
   color: "Blue"
 });

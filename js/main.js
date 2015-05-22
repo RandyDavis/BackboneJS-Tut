0357b9a3127d3643ae07b6400e5b3488ac3fe883
Randy Davis <randy_davis@outlook.com>
2015-05-22 12:19:05 -0500

    finish Handling Collection Events redo

--- js/main.js
+++ js/main.js
@@ -300,33 +300,33 @@
 
 
 // Lecture 26 redo - Handling Model Events
-var Song = Backbone.Model.extend({
-  defaults: {
-    listeners: 0
-  }
-});
-
-var SongView = Backbone.View.extend({
-  initialize: function() {
-    this.model.on("change", this.render, this);
-    // Or you could call your own function like below on the specific model change instead
-    // this.model.on("change", this.onModelChange, this);
-  },
-  
-  onModelChange: function () {
-    this.$el.addClass("someClass");
-  },
-  
-  render: function() {
-    this.$el.html(this.model.get("title") + " - Listeners: " + this.model.get("listeners"));
-    
-    return this;
-  }
-});
-
-var song = new Song({ title: "Blue in Green" });
-var songView = new SongView({ el: "#container", model: song });
-songView.render();
+//var Song = Backbone.Model.extend({
+//  defaults: {
+//    listeners: 0
+//  }
+//});
+//
+//var SongView = Backbone.View.extend({
+//  initialize: function() {
+//    this.model.on("change", this.render, this);
+//    // Or you could call your own function like below on the specific model change instead
+//    // this.model.on("change", this.onModelChange, this);
+//  },
+//  
+//  onModelChange: function () {
+//    this.$el.addClass("someClass");
+//  },
+//  
+//  render: function() {
+//    this.$el.html(this.model.get("title") + " - Listeners: " + this.model.get("listeners"));
+//    
+//    return this;
+//  }
+//});
+//
+//var song = new Song({ title: "Blue in Green" });
+//var songView = new SongView({ el: "#container", model: song });
+//songView.render();
 
 
 // Lecture 27 - Handling Collection Events
@@ -392,6 +392,77 @@ songView.render();
 // songsView.render();
 
 // // In the console enter to see changes: songs.remove(songs.at(0));
+
+
+
+//  Lecture 27 redo - Handling Collection Events
+var Song = Backbone.Model.extend();
+
+var Songs = Backbone.Collection.extend({
+  model: Song
+});
+
+var SongView = Backbone.View.extend({
+  tagName: "li",
+  
+  render: function() {
+    this.$el.html(this.model.get("title"));
+    this.$el.attr("id", this.model.id);
+    
+    return this;
+  }
+});
+
+var SongsView = Backbone.View.extend({
+  tagName: "ul",
+  
+  initialize: function() {
+    this.model.on("add", this.onSongAdded, this);
+    this.model.on("remove", this.onSongRemoved, this);
+  },
+  
+  onSongAdded: function(song) {
+    // console.log("Song Added");
+    var songView = new SongView({ model: song });
+    
+    this.$el.append(songView.render().$el);
+  },
+  
+  onSongRemoved: function(song) {
+    // console.log("Song Removed");
+    this.$el.find("li#" + song.id).remove();
+    
+    // a shorter way to write the above line of remove code
+    this.$("li#" + song.id).remove();
+  },
+  
+  render: function() {
+    var self = this;
+    this.model.each(function(song) {
+      var songView = new SongView({ model: song });
+      self.$el.append(songView.render().$el);
+    });
+  }
+});
+
+//var song = new Song({ title: "Blue in Green" });
+
+var songs = new Songs([
+  new Song({ id: 1, title: "Blue in Green" }),
+  new Song({ id: 2, title: "So What" }),
+  new Song({ id: 3, title: "All Blues" })
+]);
+
+//var songView = new SongView({ el: "#container", model: song });
+//songView.render();
+
+var songsView = new SongsView({ el: "#songs", model: songs });
+songsView.render();
+
+
+
+
+
 
 
 

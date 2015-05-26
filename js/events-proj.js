7684322576e6e8b8b542183ce2fca922c42e6a63
Randy Davis <randy_davis@outlook.com>
2015-05-26 16:47:03 -0500

    add view project code to event-proj.js file in preparation for completing the event project

---
+++ js/events-proj.js
@@ -0,0 +1,287 @@
+//var Vehicle = Backbone.Model.extend({
+//  validate: function() {
+//    if(!attrs.registrationNumber)
+//      return "Registration Number is Required!";
+//  },
+//
+//  start: function() {
+//    console.log("Vehicle has started!");
+//  },
+//
+//  defaults: {
+//    idAttribute: "registrationNumber"
+//  }
+//});
+//
+//var Vehicles = Backbone.Collection.extend({
+//  model: Vehicle
+//});
+//
+//var VehicleView = Backbone.View.extend({
+//  // initialize: function() {
+//  //   this.model.on("remove", this.onVehicleRemoved, this);
+//  // },
+//  tagName: 'li',
+//
+//  className: 'vehicle',
+//
+//  attributes: {
+//    "dataColor": "red"
+//  },
+//
+//  events: {
+//    "click #removeVehicle": "onVehicleRemoved"
+//  },
+//
+//  onVehicleRemoved: function() {
+//    console.log("Vehicle Removed");
+//    this.$("li#" + vehicles.id).remove();
+//  },
+//
+//  // onVehicleRemoved: function() {
+//  //   console.log("Vehicle Removed");
+//  //   // this.model.remove();
+//  // },
+//  
+//  render: function() {
+//    this.$el.html(this.model.get("registrationNumber") + '<a href="#" class="button round removeVehicle">Remove Vehicle</a>');
+//    this.$el.attr("id", this.model.id);
+//    return this;
+//  }
+//});
+//
+//var VehiclesView = Backbone.View.extend({
+//  tagName: "ul",
+//  initialize: function() {
+//    this.model.on("add", this.onVehicleAdded, this);
+//    this.model.on("remove", this.onVehicleRemoved, this);
+//  },
+//
+//  onVehicleAdded: function() {
+//    console.log("Vehicle Added!");
+//    var vehicleView = new VehicleView({ model: vehicle });
+//
+//    this.$el.append(vehicleView.render().el);
+//  },
+//
+//  onVehicleRemoved: function() {
+//    console.log("Vehicle Removed");
+//    this.$("li#" + vehicle.id).remove();
+//  },
+//  // events: {
+//  //   "click #removeVehicle": "onVehicleRemoved"
+//  // },
+//  render: function() {
+//    var self = this;
+//
+//    this.model.each(function(vehicle) {
+//      var vehicleView = new VehicleView({ model: vehicle });
+//      self.$el.append(vehicleView.render().$el);
+//    });
+//    // var template = _.template($("#vehiclesTemplate").html());
+//    // var html = template(this.model.toJSON());
+//    // this.$el.html(html);
+//    // this.$el.html(template(this.model.toJSON()));
+//    // return this;
+//  }
+//
+//});
+//
+//var vehicles = new Vehicles([
+//  new Vehicle({ id: 1, registrationNumber: "XLT123" }),
+//  new Vehicle({ id: 2, registrationNumber: "ZIA845" }),
+//  new Vehicle({ id: 3, registrationNumber: "PQB67R" })
+//]);
+//
+//// Remove this comment
+////  Another comment
+//
+//// Need to add code to get list of vehicles to show on .render() call
+//
+//
+//
+//var vehiclesView = new VehiclesView({ el: "#vehicles", model: vehicles});
+//vehiclesView.render();
+
+
+// Vehicle Proj Redo
+//var Vehicle = Backbone.Model.extend({
+//  defaults: {
+//    idAttribute: "registrationNumber"
+//  },
+//  
+//  validate: function(attrs) {
+//    if (!attrs.registrationNumber) {
+//      return "Registration Number is required!"
+//    }
+//  },
+//  
+//  start: function() {
+//    console.log("Vehicle has started...");
+//  }
+//});
+//
+//var Vehicles = Backbone.Collection.extend({
+//   model: Vehicle
+//});
+//
+//var VehicleView = Backbone.View.extend({
+//  // initialize: function() {}
+//  tagName: "li",
+//  
+//  className: "vehicle",
+//  
+//  attributes: {
+//    "dataColor": "red"
+//  },
+//  
+//  events: {
+//    "click button": "onClickDelete"
+//  },
+//  
+//  onClickDelete: function() {
+//    this.remove(vehicle);
+//  },
+//  
+//  render: function() {
+//    this.$el.html(this.model.get("registrationNumber") + " <button>Delete</button>");
+//  
+//    return this;
+//  }
+//});
+//
+//var VehiclesView = Backbone.View.extend({
+//  tagName: "ul",
+//  
+//  initialize: function() {
+//    this.model.on("add", this.onVehicleAdded, this);
+//    this.model.on("remove", this.onVehicleRemoved, this);
+//  },
+//  
+//  onVehicleAdded: function(vehicle) {
+//    // console.log("Vehicle Added");
+//    var vehicleView = new VehicleView({ model: vehicle });
+//    
+//    this.$el.append(vehicleView.render().$el);
+//  },
+//  
+//  onVehicleRemoved: function(vehicle) {
+//    // console.log("Vehicle Removed");
+//    this.$("li#" + vehicle.id).remove();
+//  },
+//  
+//  render: function() {
+//    var self = this;
+//    this.model.each(function(vehicle) {
+//      var vehicleView = new VehicleView({ model: vehicle });
+//      self.$el.append(vehicleView.render().$el);
+//    });
+//  }
+//});
+//
+//var vehicle = new Vehicle({ registrationNumber: "DR4-XKY", "dataColor": "green" });
+//
+//var vehicles = new Vehicles([
+//  new Vehicle({ id: 1, registrationNumber: "XXX-XX4",  "dataColor": "yellow" }),
+//  new Vehicle({ id: 2, registrationNumber: "YH5-YX4",  "dataColor": "blue" }),
+//  new Vehicle({ id: 3, registrationNumber: "H73-XJ1",  "dataColor": "red" }),
+//  new Vehicle({ id: 4, registrationNumber: "TY0-XD4",  "dataColor": "black" })
+//]);
+//
+////var vehicleView = new VehicleView({ el: "#container", model: vehicle });
+////vehicleView.render();
+//
+//var vehiclesView = new VehiclesView({ el: "#container", model: vehicles });
+//vehiclesView.render();
+
+
+
+
+// Events Proj
+var Vehicle = Backbone.Model.extend({
+	defaults: {
+		idAttribute: "registrationNumber"
+	},
+	
+	validate: function(attrs) {
+		if (!attrs.registrationNumber) {
+			return "Registration Number is required!";
+		}
+	},
+	
+	start: function() {
+		console.log("Vehicle has started...");
+	}
+});
+
+var Vehicles = Backbone.Collection.extend({
+	model: Vehicle
+});
+
+var VehicleView = Backbone.View.extend({
+	tagName: "li",
+	
+	className: "vehicle",
+	
+	attributes: {
+		dataColor: "red"
+	},
+	
+	events: {
+		"click button": "onClickDelete"
+	},
+	
+	onClickDelete: function() {
+		this.remove(vehicle);
+	},
+	
+	render: function() {
+		this.$el.html(this.model.get("registrationNumber") + " <button>Delete</button>");
+		
+		return this;
+	}
+});
+
+var VehiclesView = Backbone.View.extend({
+	tagName: "ul",
+	
+	initialize: function() {
+		this.model.on("add", this.onVehicleAdded, this);
+		this.model.on("remove", this.onVehicleRemoved, this);
+	},
+	
+	onVehicleAdded: function(vehicle) {
+		// console.log("Vehicle Added");
+		var vehicleView = new VehicleView({ model: vehicle });
+		
+		this.$el.append(vehicleView.render().$el);
+	},
+	
+	onVehicleRemoved: function(vehicle) {
+		// console.log("Vehicle Removed");
+		this.$("li#" + vehicle.id).remove();
+	},
+	
+	render: function() {
+		var self = this;
+		this.model.each(function(vehicle) {
+			var vehicleView = new VehicleView({ model: vehicle });
+			self.$el.append(vehicleView.render().$el);
+		});
+	}
+});
+
+var vehicle = new Vehicle({ registrationNumber: "GOF-AST", dataColor: "yellow" });
+
+var vehicles = new Vehicles([
+	new Vehicle({ id: 1, registrationNumber: "FB8-11A", dataColor: "white" }),
+	new Vehicle({ id: 2, registrationNumber: "FBR-POA", dataColor: "blue" }),
+	new Vehicle({ id: 3, registrationNumber: "XBE-MAN", dataColor: "pink" }),
+	new Vehicle({ id: 4, registrationNumber: "3GP-01M", dataColor: "navy" })
+]);
+
+//var vehicleView = new VehicleView({ el: "#container", model: vehicle });
+//vehicleView.render();
+
+var vehiclesView = new VehiclesView({ el: "#container", model: vehicles });
+vehiclesView.render();
